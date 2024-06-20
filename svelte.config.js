b729823fc7564ac07305ebcb64b6c49c9f457ffe
olli <144932831+OLILHR@users.noreply.github.com>
2024-06-20 10:36:31 +0200

    ➕ Add Prettier dev dependency and CI action

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,19 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from "@sveltejs/adapter-static";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-
-    kit: {
-        adapter: adapter({
-            pages: 'build',
-            assets: 'build',
-            fallback: "404.html",
-            precompress: false,
-            strict: true,
-        }),
-        paths: {
-            base: process.argv.includes("dev") ? "" : process.env.BASE_PATH
-        }
-    }
+  kit: {
+    adapter: adapter({
+      pages: "build",
+      assets: "build",
+      fallback: "404.html",
+      precompress: false,
+      strict: true,
+    }),
+    paths: {
+      base: process.argv.includes("dev") ? "" : process.env.BASE_PATH,
+    },
+  },
 };
 
 export default config;
