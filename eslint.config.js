6f1f0dc9d897c0c928a94d71ca0bab90a1a9b25f
olli <info@oliverlahr.de>
2024-10-08 08:39:36 +0200

    Replace `.eslintignore` with `eslint.config.js`

---
+++ eslint.config.js
@@ -0,0 +1,12 @@
+export default [
+  {
+    ignores: [
+      "package.json",
+      "package-lock.json",
+      ".svelte-kit/*",
+      "node_modules/*",
+      "test-results/*",
+      "build/*",
+    ],
+  },
+];
