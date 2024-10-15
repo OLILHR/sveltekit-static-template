b8322b15954ab5a843899f9ece43a2347a4c9c6b
olli <info@oliverlahr.de>
2024-10-15 12:11:23 +0200

    Declare eslint import rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,42 +1,41 @@
 module.exports = {
-    root: true,
-    extends: [
-      "eslint:recommended",
-      "plugin:@typescript-eslint/recommended",
-      "plugin:svelte/recommended",
-    ],
-    parser: "@typescript-eslint/parser",
-    plugins: ["@typescript-eslint", "import"],
-    parserOptions: {
-      sourceType: "module",
-      ecmaVersion: 2020,
-      extraFileExtensions: [".svelte"],
-    },
-    overrides: [
-      {
-        files: ["*.svelte"],
-        parser: "svelte-eslint-parser",
-        parserOptions: {
-          parser: {
-            ts: "@typescript-eslint/parser",
-            js: "espree",
-            typescript: "@typescript-eslint/parser",
-          },
+  root: true,
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:svelte/recommended",
+    "prettier"
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "simple-import-sort"],
+  parserOptions: {
+    sourceType: "module",
+    ecmaVersion: 2020,
+    extraFileExtensions: [".svelte"],
+  },
+  overrides: [
+    {
+      files: ["*.svelte"],
+      parser: "svelte-eslint-parser",
+      parserOptions: {
+        parser: {
+          ts: "@typescript-eslint/parser",
+          js: "espree",
+          typescript: "@typescript-eslint/parser",
         },
       },
-    ],
-    env: {
-      browser: true,
-      es2017: true,
-      node: true,
     },
-    rules: {
-      "import/order": [ 
-          "error", 
-          { 
-              "groups": ["builtin", "external", "internal"],
-              "alphabetize": { "order": "asc", "caseInsensitive": true } 
-          } 
-      ]
-    },
-  }
+  ],
+  env: {
+    browser: true,
+    es2017: true,
+    node: true,
+  },
+  rules: {
+    "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error",
+    "import/first": "error",
+    "import/newline-after-import": "error",
+    "import/no-duplicates": "error"
+  },
+}
